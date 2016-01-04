de22e58ab06afbb49ba6641fd14d28ae711ea1a7
cgates <cgates@umich.edu>
2016-01-04 12:30:24 -0500

    Added argparser and help

--- ampliconsoftclipper/clipper.py
+++ ampliconsoftclipper/clipper.py
@@ -1,23 +1,63 @@
 #! /usr/bin/env python
-"""Softclips primers at edge of aligned reads"""
+"""Softclips primers at edge of aligned reads based on primer locations; emits
+new BAM file with clipped reads optinoally excluding alignments that did not
+match primer locations."""
+
+##   Copyright 2014 Bioinformatics Core, University of Michigan
+##
+##   Licensed under the Apache License, Version 2.0 (the "License");
+##   you may not use this file except in compliance with the License.
+##   You may obtain a copy of the License at
+##
+##       http://www.apache.org/licenses/LICENSE-2.0
+##
+##   Unless required by applicable law or agreed to in writing, software
+##   distributed under the License is distributed on an "AS IS" BASIS,
+##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##   See the License for the specific language governing permissions and
+##   limitations under the License.
+
 #TODO: elaborate module doc
 from __future__ import print_function, absolute_import, division
 
+import argparse
 import csv
 from datetime import datetime
-import os
 import pysam
 import sys
 import traceback
+
 import ampliconsoftclipper
 import ampliconsoftclipper.cigar as cigar
 import ampliconsoftclipper.readhandler as readhandler
 from ampliconsoftclipper.util import PrimerStats, PrimerStatsDumper,\
         PrimerPair, Read
 
-__VERSION__ = ampliconsoftclipper.__version__
+__version__ = ampliconsoftclipper.__version__
 
-#TODO: rename private classes to omit underscore
+DESCRIPTION=\
+'''Match each alignment in input BAM to primer, softclipping the primer region.
+
+This helps identify variants that land in the primer region because sequences
+which start or end with the primer region are only measuring the efficacy of
+the sample prep and the presence of those alignments tend to overwhelm true
+variants which occur in that region measured by overlapping primers. The output
+is conceptually similar to clipping the primers from the FASTQ reads but
+preserves the primers during alignment to improve alignment quality.
+'''
+
+class _ClipperUsageError(Exception):
+    """Raised for malformed command or invalid arguments."""
+    def __init__(self, msg, *args):
+        super(_ClipperUsageError, self).__init__(msg, *args)
+
+
+class _ClipperArgumentParser(argparse.ArgumentParser):
+    """Argument parser that raises UsageError instead of exiting."""
+    #pylint: disable=too-few-public-methods
+    def error(self, message):
+        '''Suppress default exit behavior'''
+        raise _ClipperUsageError(message)
 
 
 #TODO: make this a logger object that writes to file and console and supports debug and info calls
@@ -89,53 +129,74 @@ def _build_handlers(input_bam_filename,
     return handlers
 
 #TODO: test
+def _parse_command_line_args(arguments):
+    parser = _ClipperArgumentParser( \
+        formatter_class=argparse.RawTextHelpFormatter,
+        usage="clipper primer_manifest input_bam output_bam",
+        description=(DESCRIPTION))
+
+    parser.add_argument("-V",
+                        "--version",
+                        action='version',
+                        version=__version__)
+    parser.add_argument('primer_manifest',
+                        help="path to primer manifest (tab-separated text)")
+    parser.add_argument('input_bam',
+                        help="path to input BAM")
+    parser.add_argument('output_bam',
+                        help="path to output BAM")
+    args = parser.parse_args(arguments)
+    return args
+
+#TODO: test
 #TODO: allow suppress/divert unmatched reads
 #TODO: deal if input bam missing index
 #TODO: deal if input bam regions disjoint with primer regions
 #TODO: warn/stop if less than 5% reads transformed
-#TODO: argparse
 def main(command_line_args=None):
-
-    if not command_line_args:
-        command_line_args = sys.argv
-    if len(command_line_args) != 4:
-        usage = "usage: {0} [thunderbolt_manifest] [input_bam] [output_bam]"
-        print(usage.format(os.path.basename(command_line_args[0])),
-              file=sys.stderr)
-        sys.exit(1)
-    (input_primer_manifest_filename,
-     input_bam_filename,
-     output_bam_filename) = command_line_args[1:]
-
-    #pylint: disable=no-member
-    _log("Reading primer pairs from [{}]", input_primer_manifest_filename)
-    with open(input_primer_manifest_filename, "r") as input_primer_manifest:
-        _initialize_primer_pairs(input_primer_manifest)
-    _log("Read [{}] primer pairs", len(PrimerPair._all_primers))
-    input_bamfile = None
+    '''Clipper entry point.'''
     try:
-        _log("Reading alignments from BAM [{}]", input_bam_filename)
-        input_bamfile = pysam.AlignmentFile(input_bam_filename,"rb")
+        if not command_line_args:
+            command_line_args = sys.argv
+        args = _parse_command_line_args(command_line_args[1:])
+
+        _log("Reading primer pairs from [{}]", args.primer_manifest)
+        with open(args.primer_manifest, "r") as input_primer_manifest:
+            _initialize_primer_pairs(input_primer_manifest)
+        _log("Read [{}] primer pairs", len(PrimerPair._all_primers))
+        input_bamfile = None
+
+        _log("Building transformations from BAM [{}]", args.input_bam)
+        #pylint: disable=no-member
+        input_bamfile = pysam.AlignmentFile(args.input_bam,"rb")
         aligned_segment_iter = input_bamfile.fetch()
         read_iter = Read.iter(aligned_segment_iter)
-        _log("Building transformations")
         read_transformations = _build_read_transformations(read_iter)
 
-        _log("Writing transformed alignments to [{}]", output_bam_filename)
-        handlers = _build_handlers(input_bam_filename,
-                                   output_bam_filename,
+        _log("Writing transformed alignments to [{}]", args.output_bam)
+        handlers = _build_handlers(args.input_bam,
+                                   args.output_bam,
                                    True)
         aligned_segment_iter = input_bamfile.fetch()
         read_iter = Read.iter(aligned_segment_iter)
         _handle_reads(handlers, read_iter, read_transformations)
+
+        _log("Done")
+    except _ClipperUsageError as usage_error:
+        message = "clipper usage problem: {}".format(str(usage_error))
+        print(message, file=sys.stderr)
+        print("See 'clipper --help'.", file=sys.stderr)
+        sys.exit(1)
     except Exception: #pylint: disable=broad-except
         _log("ERROR: An unexpected error occurred")
         _log(traceback.format_exc())
         exit(1)
     finally:
-        if input_bamfile:
-            input_bamfile.close()
-    _log("Done")
+        try:
+            if input_bamfile:
+                input_bamfile.close()
+        except NameError:
+            pass
 
 
 if __name__ == '__main__':
